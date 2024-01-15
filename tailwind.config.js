90060d2772125e611a02e824c220630968d29890
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-01-15 19:58:46 +0530

    Added inter font in tailwind.config.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./src/stories/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "inter": ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
-
