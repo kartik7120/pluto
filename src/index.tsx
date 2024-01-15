885badb448df5e7648ff5f947ee3c9e822ecb9bc
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-01-15 20:01:30 +0530

    Removed irrelevant text from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
 import reactDom from "react-dom";
 import React from "react";
 import "./main.css";
-type c = React.ReactElement;
-
-export default function index() {
-  return (
-    <div>index</div>
-  )
-}
